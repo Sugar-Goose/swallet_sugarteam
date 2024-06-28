aac6cceaabc18d9be9aefbe9d91d0f24d8f33070
Sugar Goose <130372150+Sugar-Goose@users.noreply.github.com>
2024-06-29 00:27:28 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,36 +1,74 @@
-document.getElementById('agreeCheckbox').addEventListener('change', function() {
-    const isChecked = this.checked;
-    const createWalletBtn = document.getElementById('createWalletBtn');
-    const importWalletBtn = document.getElementById('importWalletBtn');
+const startPage = document.querySelector("#startPage")
+const pinPage = document.querySelector("#pin")
 
-    if (isChecked) {
-        createWalletBtn.classList.remove('disabled');
-        importWalletBtn.classList.remove('disabled');
-    } else {
-        createWalletBtn.classList.add('disabled');
-        importWalletBtn.classList.add('disabled');
-    }
-});
+if (startPage) {
+    document.getElementById('agreeCheckbox').addEventListener('change', function() {
+        const isChecked = this.checked;
+        const createWalletBtn = document.getElementById('createWalletBtn');
+        const importWalletBtn = document.getElementById('importWalletBtn');
+    
+        if (isChecked) {
+            createWalletBtn.classList.remove('disabled');
+            importWalletBtn.classList.remove('disabled');
+        } else {
+            createWalletBtn.classList.add('disabled');
+            importWalletBtn.classList.add('disabled');
+        }
+    });
 
+    const overlay = document.querySelector('.disclaimer__overlay');
+    const content1 = document.querySelector('.logo');
+    const content2 = document.querySelector('.header');
+    const content3 = document.querySelector('.checkbox');
+    const content4 = document.querySelector('.buttons');
 
-const overlay = document.querySelector('.disclaimer__overlay');
-const content1 = document.querySelector('.logo');
-const content2 = document.querySelector('.header');
-const content3 = document.querySelector('.checkbox');
-const content4 = document.querySelector('.buttons');
+    document.getElementById('openDisclaimerBtn').addEventListener('click', () => {
+        overlay.classList.add("active")
+        content1.classList.add("hidden")
+        content2.classList.add("hidden")
+        content3.classList.add("hidden")
+        content4.classList.add("hidden")
+    });
 
-document.getElementById('openDisclaimerBtn').addEventListener('click', () => {
-    overlay.classList.add("active")
-    content1.classList.add("hidden")
-    content2.classList.add("hidden")
-    content3.classList.add("hidden")
-    content4.classList.add("hidden")
-});
+    document.getElementById('closeDisclaimerBtn').addEventListener('click', () => {
+        overlay.classList.remove("active")
+        content1.classList.remove("hidden")
+        content2.classList.remove("hidden")
+        content3.classList.remove("hidden")
+        content4.classList.remove("hidden")
+    });
 
-document.getElementById('closeDisclaimerBtn').addEventListener('click', () => {
-    overlay.classList.remove("active")
-    content1.classList.remove("hidden")
-    content2.classList.remove("hidden")
-    content3.classList.remove("hidden")
-    content4.classList.remove("hidden")
-});
+}
+
+if (pinPage) {
+    document.addEventListener('DOMContentLoaded', function() {
+        const buttons = document.querySelectorAll('.kb_button:not(.none):not(.backspace)');
+        const dots = document.querySelectorAll('.dot');
+        const backspaceButton = document.querySelector('.kb_button.backspace');
+        const header = document.getElementById('header');
+        let activeIndex = 0;
+
+        buttons.forEach(button => {
+            button.addEventListener('click', () => {
+                if (activeIndex < dots.length) {
+                    dots[activeIndex].classList.add('active');
+                    activeIndex++;
+                    if (activeIndex === dots.length) {
+                        header.textContent = 'Repeat the code';
+                        setTimeout(() => {
+                            dots.forEach(dot => dot.classList.remove('active'));
+                            activeIndex = 0;
+                        }, 1000);
+                    }
+                }
+            });
+        });
+
+        backspaceButton.addEventListener('click', () => {
+            if (activeIndex > 0) {
+                activeIndex--;
+                dots[activeIndex].classList.remove('active');
+            }
+        });
+    });
+}
