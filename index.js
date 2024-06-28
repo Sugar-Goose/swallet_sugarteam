0dd62ee1855a80f83a6503b8a7c22d433e9328d6
Sugar Goose <130372150+Sugar-Goose@users.noreply.github.com>
2024-06-28 18:46:05 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -11,3 +11,26 @@ document.getElementById('agreeCheckbox').addEventListener('change', function() {
         importWalletBtn.classList.add('disabled');
     }
 });
+
+
+const overlay = document.querySelector('.disclaimer__overlay');
+const content1 = document.querySelector('.logo');
+const content2 = document.querySelector('.header');
+const content3 = document.querySelector('.checkbox');
+const content4 = document.querySelector('.buttons');
+
+document.getElementById('openDisclaimerBtn').addEventListener('click', () => {
+    overlay.classList.add("active")
+    content1.classList.add("hidden")
+    content2.classList.add("hidden")
+    content3.classList.add("hidden")
+    content4.classList.add("hidden")
+});
+
+document.getElementById('closeDisclaimerBtn').addEventListener('click', () => {
+    overlay.classList.remove("active")
+    content1.classList.remove("hidden")
+    content2.classList.remove("hidden")
+    content3.classList.remove("hidden")
+    content4.classList.remove("hidden")
+});
