be8609b7a3ee8c29c21065df0162cb2fc44c852c
Sugar Goose <130372150+Sugar-Goose@users.noreply.github.com>
2024-07-08 21:13:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// FrontEnd
+// Фронтенд
 const startPage = document.querySelector("#startPage");
 const createWalletPage = document.querySelector("#createWalletPage");
 const sercretPhrasePage = document.querySelector("#sercretPhrasePage");
@@ -9,58 +9,130 @@ const privateKeyPage = document.querySelector("#privateKey");
 const secretPhraseShowPage = document.querySelector("#secretPhraseShow");
 const settingsPage = document.querySelector("#settings");
 
-// Common Function
-function fetchData(apiUrl, successCallback, errorCallback) {
-    fetch(apiUrl, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json"
+if (startPage) {
+    document.getElementById('agreeCheckbox').addEventListener('change', function() {
+        const isChecked = this.checked;
+        const createWalletBtn = document.getElementById('createWalletBtn');
+        const importWalletBtn = document.getElementById('importWalletBtn');
+    
+        if (isChecked) {
+            createWalletBtn.classList.remove('disabled');
+            importWalletBtn.classList.remove('disabled');
+        } else {
+            createWalletBtn.classList.add('disabled');
+            importWalletBtn.classList.add('disabled');
         }
-    })
-    .then(response => response.json())
-    .then(successCallback)
-    .catch(errorCallback);
+    });
+
+    const overlay = document.querySelector('.disclaimer__overlay');
+    const content1 = document.querySelector('.logo');
+    const content2 = document.querySelector('.header');
+    const content3 = document.querySelector('.checkbox');
+    const content4 = document.querySelector('.buttons');
+
+    document.getElementById('openDisclaimerBtn').addEventListener('click', () => {
+        overlay.classList.add("active")
+        content1.classList.add("hidden")
+        content2.classList.add("hidden")
+        content3.classList.add("hidden")
+        content4.classList.add("hidden")
+    });
+
+    document.getElementById('closeDisclaimerBtn').addEventListener('click', () => {
+        overlay.classList.remove("active")
+        content1.classList.remove("hidden")
+        content2.classList.remove("hidden")
+        content3.classList.remove("hidden")
+        content4.classList.remove("hidden")
+    });
+
 }
 
-function formatPrice(price) {
-    return price.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+if (mainPage) {
+    document.addEventListener('DOMContentLoaded', function() {
+        const assetsBtn = document.getElementById('assetsBtn');
+        const historyBtn = document.getElementById('historyBtn');
+        const slider = document.getElementById('slider');
+        const historyTab = document.querySelector('.history__tab');
+        const assetsTab = document.querySelector(".assets");
+    
+        assetsBtn.addEventListener('click', () => {
+            slider.style.transform = 'translateX(0)';
+            assetsBtn.classList.add('active');
+            assetsTab.classList.add('active');
+            historyBtn.classList.remove('active');
+            historyTab.classList.remove('active')
+        });
+    
+        historyBtn.addEventListener('click', () => {
+            slider.style.transform = 'translateX(100%)';
+            assetsBtn.classList.remove('active');
+            assetsTab.classList.remove('active');
+            historyBtn.classList.add('active');
+            historyTab.classList.add('active')
+        });
+
+        
+        let usernameHeader = document.getElementById('username');
+        let userLastNameHeader = document.getElementById('lastname');
+
+        usernameHeader.innerHTML = user_id;
+        userLastNameHeader.innerHTML = user_lastname;
+    });
 }
 
-function updatePrice(price, elementId) {
-    const element = document.getElementById(elementId);
-    if (element) {
-        element.innerHTML = price;
-    }
+if (privateKeyPage) {
+    document.getElementById('privateKeyCopy').addEventListener('click', function() {
+        let privateKeyText = document.querySelector('.private_key_block p').innerHTML;
+        privateKeyText = privateKeyText.replace(/<br>/g, '');
+
+        const tempElement = document.createElement('textarea');
+        tempElement.value = privateKeyText;
+        document.body.appendChild(tempElement);
+        tempElement.select();
+        document.execCommand('copy');
+        document.body.removeChild(tempElement);
+
+        document.getElementById("privateKeyCopy").innerHTML = "Copied!"
+        document.getElementById("privateKeyCopy").style.color = "#51df7b"
+    });
 }
 
-// Fetch and Update Prices
-const prices = {
-    BTC: "https://api.diadata.org/v1/assetQuotation/Bitcoin/0x0000000000000000000000000000000000000000",
-    USDT: "https://api.diadata.org/v1/assetQuotation/Ethereum/0xdAC17F958D2ee523a2206206994597C13D831ec7",
-    TRX: "https://api.diadata.org/v1/assetQuotation/Tron/0x0000000000000000000000000000000000000000",
-    BNB: "https://api.diadata.org/v1/assetQuotation/BinanceSmartChain/0x0000000000000000000000000000000000000000",
-    BCH: "https://api.diadata.org/v1/assetQuotation/BitcoinCash/0x0000000000000000000000000000000000000000",
-    ETH: "https://api.diadata.org/v1/assetQuotation/Ethereum/0x0000000000000000000000000000000000000000",
-    SOL: "https://api.diadata.org/v1/assetQuotation/Solana/0x0000000000000000000000000000000000000000",
-    ATOM: "https://api.diadata.org/v1/assetQuotation/Cosmos/0x0000000000000000000000000000000000000000",
-    BUSD: "https://api.diadata.org/v1/assetQuotation/BinanceSmartChain/0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56",
-    LTC: "https://api.diadata.org/v1/assetQuotation/Litecoin/0x0000000000000000000000000000000000000000"
-};
+if (secretPhraseShowPage) {
+    document.getElementById('privateKeyCopy').addEventListener('click', function() {
+        const wordElements = document.querySelectorAll('.word span');
+        const words = Array.from(wordElements).map(el => el.textContent).join(' ');
+        const tempElement = document.createElement('textarea');
+        tempElement.value = words;
+        document.body.appendChild(tempElement);
+        tempElement.select();
+        document.execCommand('copy');
+        document.body.removeChild(tempElement);
+    
+        document.getElementById("privateKeyCopy").innerHTML = "Copied!"
+        document.getElementById("privateKeyCopy").style.color = "#51df7b"
+    }); 
+}
 
-Object.keys(prices).forEach(key => {
-    fetchData(prices[key],
-        function(data) {
-            const formattedPrice = formatPrice(data.Price);
-            updatePrice(formattedPrice, key.toLowerCase() + "Price");
-        },
-        function(error) {
-            console.error(error);
-        }
-    );
-});
+if (settingsPage) {
+    const overlay = document.querySelector('.disclaimer__overlay');
+    const content1 = document.querySelector('.assets');
+    const content2 = document.querySelector('.bottom__menu');
+
+    document.getElementById('openDisclaimerBtn').addEventListener('click', () => {
+        overlay.classList.add("active")
+        content1.classList.add("hidden")
+        content2.classList.add("hidden")
+    });
+
+    document.getElementById('closeDisclaimerBtn').addEventListener('click', () => {
+        overlay.classList.remove("active")
+        content1.classList.remove("hidden")
+        content2.classList.remove("hidden")
+    });
+}
 
 if (startPage) {
-    // Handle checkbox and buttons enabling/disabling
     document.getElementById('agreeCheckbox').addEventListener('change', function() {
         const isChecked = this.checked;
         const createWalletBtn = document.getElementById('createWalletBtn');
@@ -98,11 +170,61 @@ if (startPage) {
     });
 }
 
-// Telegram
+// Бекенд
+function fetchData(apiUrl, successCallback, errorCallback) {
+    fetch(apiUrl, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    .then(response => response.json())
+    .then(successCallback)
+    .catch(errorCallback);
+}
+
+function formatPrice(price) {
+    return price.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+}
+
+function updatePrice(price, elementId) {
+    const element = document.getElementById(elementId);
+    if (element) {
+        element.innerHTML = price;
+    }
+}
+
+// Получение и обновление цен на монеты
+const prices = {
+    BTC: "https://api.diadata.org/v1/assetQuotation/Bitcoin/0x0000000000000000000000000000000000000000",
+    USDT: "https://api.diadata.org/v1/assetQuotation/Ethereum/0xdAC17F958D2ee523a2206206994597C13D831ec7",
+    TRX: "https://api.diadata.org/v1/assetQuotation/Tron/0x0000000000000000000000000000000000000000",
+    BNB: "https://api.diadata.org/v1/assetQuotation/BinanceSmartChain/0x0000000000000000000000000000000000000000",
+    BCH: "https://api.diadata.org/v1/assetQuotation/BitcoinCash/0x0000000000000000000000000000000000000000",
+    ETH: "https://api.diadata.org/v1/assetQuotation/Ethereum/0x0000000000000000000000000000000000000000",
+    SOL: "https://api.diadata.org/v1/assetQuotation/Solana/0x0000000000000000000000000000000000000000",
+    ATOM: "https://api.diadata.org/v1/assetQuotation/Cosmos/0x0000000000000000000000000000000000000000",
+    BUSD: "https://api.diadata.org/v1/assetQuotation/BinanceSmartChain/0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56",
+    LTC: "https://api.diadata.org/v1/assetQuotation/Litecoin/0x0000000000000000000000000000000000000000"
+};
+
+Object.keys(prices).forEach(key => {
+    fetchData(prices[key],
+        function(data) {
+            const formattedPrice = formatPrice(data.Price);
+            updatePrice(formattedPrice, key.toLowerCase() + "Price");
+        },
+        function(error) {
+            console.error(error);
+        }
+    );
+});
+
+// Обьявление телеграма
 const tg = window.Telegram.WebApp;
 const user = tg.initDataUnsafe.user;
 
-// User Login and Logout
+// Логин и логаут
 function updateStep(step) {
     fetch('http://localhost:5000/api/updateStep', {
         method: 'POST',
@@ -128,29 +250,13 @@ if (startPage) {
 }
 
 if (settingsPage) {
-    let disclaimer__overlay = document.querySelector(".disclaimer__overlay");
-    let assets = document.querySelector(".assets");
-    document.querySelector("#openDisclaimerBtn").addEventListener("click", () => {
-        disclaimer__overlay.classList.add("active");
-        assets.classList.remove("active");
-    });
-
-    document.querySelector("#closeDisclaimerBtn").addEventListener("click", () => {
-        disclaimer__overlay.classList.remove("active");
-        assets.classList.add("active");
-    });
-
     document.querySelector("#logout-button").addEventListener("click", () => {
         updateStep(0);
         window.location.href = 'https://sugar-goose.github.io/swallet_sugarteam/';
     });
-
-    document.querySelector("#supportButton").addEventListener("click", () => {
-        window.location.href = 'https://t.me/swallet_support_bot';
-    });
 }
 
-// Create PIN Code and Save to Database
+// Создание пин кода, и сохранение его в базу
 if (pinPage) {
     document.addEventListener('DOMContentLoaded', function() {
         const buttons = document.querySelectorAll('.kb_button:not(.none):not(.backspace)');
@@ -226,7 +332,7 @@ if (pinPage) {
     });
 }
 
-// Create Wallet in Database
+// Создание кошелька в бд
 if (startPage) {
     document.querySelector("#createWalletBtn").addEventListener("click", () => {
         const user_id = user.id;
@@ -244,7 +350,7 @@ if (startPage) {
     });
 }
 
-// Display User Data
+// Отображение данных пользователя
 if (mainPage) {
     const prices = {
         btc: "https://api.diadata.org/v1/assetQuotation/Bitcoin/0x0000000000000000000000000000000000000000",
@@ -331,6 +437,19 @@ if (mainPage) {
 
 // Отображение секретной фразы при регистрации
 if (sercretPhrasePage) {
+    fetch(`http://localhost:5000/api/user/${user.id}`)
+    .then(response => response.json())
+    .then(userData => {
+        const words = userData.phrase.split(" ");
+        for (let i = 0; i < words.length; i++) {
+            document.querySelector(`#word${i + 1}`).innerHTML = words[i];
+        }
+    })
+    .catch(error => console.error('Error:', error));
+}
+
+// Отображение секретной фразы на странице фразы
+if (secretPhraseShowPage) {
     fetch(`http://localhost:5000/api/user/${user.id}`)
     .then(response => response.json())
     .then(userData => {
