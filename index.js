7b84b1106cd235df82910246f5945c2149d2fb34
Sugar Goose <130372150+Sugar-Goose@users.noreply.github.com>
2024-07-18 17:58:21 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -321,6 +321,56 @@ Object.keys(prices).forEach(key => {
 // Обьявление телеграма
 const tg = window.Telegram.WebApp;
 const user = tg.initDataUnsafe.user;
+const username = user.username;
+
+// Отображение транзакций
+async function fetchTransactions(username) {
+    try {
+        const response = await fetch(`http://localhost:5000/api/transactions?username=${username}`);
+        const transactions = await response.json();
+        return transactions;
+    } catch (error) {
+        console.error('Error fetching transactions:', error);
+    }
+}
+
+function displayTransactions(transactions) {
+    const historyTab = document.querySelector('.history__tab');
+    historyTab.innerHTML = ''; // Очищаем предыдущие данные
+
+    transactions.forEach((transaction, index) => {
+        const isIncoming = transaction.to === username;
+        const transactionClass = isIncoming ? 'incoming' : 'outcoming';
+        const sign = isIncoming ? '+' : '-';
+
+        const transactionHTML = `
+            <div class="transcation ${transactionClass}">
+                <div class="transaction__left">
+                    <h4>From: <span>${isIncoming ? transaction.from : 'You'}</span></h4>
+                    <h4><span>${transaction.time}</span> UTC+2</h4>
+                    <h3 class="transaction___sum">${sign} <span>${transaction.amount}</span> <span>${transaction.currency}</span></h3>
+                </div>
+                <div class="transaction__right">
+                    <h4>To: <span>${isIncoming ? 'You' : transaction.to}</span></h4>
+                </div>
+            </div>
+            <hr>
+        `;
+        historyTab.innerHTML += transactionHTML;
+    });
+}
+
+if (mainPage) {
+    document.addEventListener('DOMContentLoaded', async () => {
+        const transactions = await fetchTransactions(username);
+        if (transactions && transactions.length > 0) {
+            displayTransactions(transactions);
+        } else {
+            document.querySelector('.history__tab').innerHTML = '<h3>History is empty</h3>';
+        }
+    });
+    
+}
 
 // Логин и логаут
 function updateStep(step) {
