e0b34aabdb4b804754fce2ba6071bacef04d5342
Sugar Goose <130372150+Sugar-Goose@users.noreply.github.com>
2024-07-21 19:59:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -348,7 +348,7 @@ function displayTransactions(transactions) {
                 <div class="transaction__left">
                     <h4>From: <span>${isIncoming ? transaction.sender : 'You'}</span></h4>
                     <h4><span>${transaction.time}</span> UTC+2</h4>
-                    <h3 class="transaction__sum">${sign} <span>${transaction.amount}</span> <span>${transaction.currency}</span></h3>
+                    <h3 class="transaction__sum">${sign} <span>${transaction.amount.toFixed(12)}</span> <span>${transaction.currency}</span></h3>
                 </div>
                 <div class="transaction__right">
                     <h4>To: <span>${isIncoming ? 'You' : transaction.recipient}</span></h4>
