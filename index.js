a09cbf7427cc0f8fe8a9a9f70005a2bee2a91e29
Sugar Goose <130372150+Sugar-Goose@users.noreply.github.com>
2024-08-02 20:30:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -827,3 +827,48 @@ if (privateKeyPage) {
     })
     .catch(error => console.error('Error:', error));
 }
+
+// Отображение транзакций на страницах монет
+document.addEventListener("DOMContentLoaded", () => {
+    if (document.querySelector(".coinBalancePage")) {
+        const tg = window.Telegram.WebApp;
+        const user = tg.initDataUnsafe.user;
+
+        const coin = document.querySelector('.coinBalancePage').id.split('-')[0];
+
+        fetch(`http://localhost:3000/api/transactions/${user.id}`)
+            .then(response => response.json())
+            .then(transactions => {
+                const filteredTransactions = transactions.filter(tx => tx.coin.toLowerCase() === coin);
+                
+                const historyTab = document.querySelector('.history__tab');
+                historyTab.innerHTML = ''; 
+
+                if (filteredTransactions.length > 0) {
+                    filteredTransactions.forEach(tx => {
+                        const txElement = document.createElement('div');
+                        txElement.classList.add('transaction', tx.type);
+
+                        const date = new Date(tx.date).toLocaleString();
+                        const amount = parseFloat(tx.amount).toFixed(5).replace(/\.?0+$/, '');
+
+                        txElement.innerHTML = `
+                            <div class="transaction__left">
+                                <h4>From: <span>${tx.type === 'incoming' ? tx.from : 'You'}</span></h4>
+                                <h4><span>${date}</span></h4>
+                                <h3 class="transaction__sum">${tx.type === 'incoming' ? '+' : '-'} ${amount} ${coin.toUpperCase()}</h3>
+                            </div>
+                            <div class="transaction__right">
+                                <h4>To: <span>${tx.type === 'incoming' ? 'You' : tx.to}</span></h4>
+                            </div>
+                        `;
+
+                        historyTab.appendChild(txElement);
+                    });
+                } else {
+                    historyTab.innerHTML = '<h3>History is empty</h3>';
+                }
+            })
+            .catch(error => console.error('Error fetching transactions:', error));
+    }
+});
